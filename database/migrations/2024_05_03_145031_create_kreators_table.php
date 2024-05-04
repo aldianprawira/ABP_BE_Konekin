9637048296295a534120142e40ea03f73f5bb1ff
Fathan <askarfathan@gmail.com>
2024-05-04 11:31:07 +0700

    delete column password di kreators dan audiens table

--- database/migrations/2024_05_03_145031_create_kreators_table.php
+++ database/migrations/2024_05_03_145031_create_kreators_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('kreators', function (Blueprint $table) {
             $table->id();
             // $table->string('idKreator',50)->primary();
-            $table->string('password', 50);
             $table->string('username', 50);
             $table->string('noHP', 50);
             $table->string('email', 50);
--- database/migrations/2024_05_03_151142_create_audiens_table.php
+++ database/migrations/2024_05_03_151142_create_audiens_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('audiens', function (Blueprint $table) {
             $table->id();
             // $table->string('idAudiens',50)->primary();
-            $table->string('password', 50);
             $table->string('username', 50);
             $table->string('noHP', 50);
             $table->string('email', 50);
